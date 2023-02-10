8191d1014515ef801c1b0b75ae9160f5abdb5d1b
So-Yeon Lee <sylee@t3q.com>
2023-02-10 18:01:03 +0900

    date 02.10 오늘은 시각화만 했다

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -125,26 +125,85 @@ df.columns
 
 # -1는 null을 의미함
 df = df.fillna(-1)
-for d in df:
-    print(d)
-    break
-
-df.isnull() == False
-
-# 지금 볼 것은 y값에 따른 null의 유무 
-hue : y
 
 
-sns.countplot(data = df, hue = "Y_Class", x = "X_1")
+for col in df.columns[6:]:
+    df[col].loc[df[col].notnull()] = 0
+    df[col].loc[df[col].isnull()] = 1
+
+sns.countplot(data = df, x = "Y_Class", hue = "X_1")
+
+fig, axis  = plt.subplots(10,10, figsize=(50,50))
+fig.subplots_adjust(wspace=0.7, hspace=0.7)
+
+columns = df.columns[6:]
+
+def X_Y_null_plot(df, columns):
+    fig, axis  = plt.subplots(10,10, figsize=(50,50))
+    fig.subplots_adjust(wspace=0.7, hspace=0.7)
+
+    k =0
+    for i in range(10):
+        for j in range(10):
+            sns.scatterplot(data=df, x="Y_Class", y = columns[k])
+            , ax=axis[i, j])
+            k += 1
+    plt.show()
+
+col = list()
+for i in range(29):
+    col.append(columns[i*100:(i+1)*100])
+
+for c in col:
+    X_Y_null_plot(df, c)
 
 
+상관관계 plot 
+imputation 중 MICE 
+Explainable ML의 관점
+
+# df = pd.concat([train.iloc[:,6:], test.iloc[:,4:]], axis=1)
+
+df1 = pd.DataFrame(list(df[df["Y_Class"]==0].sum(axis=0))[6:], columns = ["0"], index = df.columns[6:])
+df1["1"] = list(df[df["Y_Class"]==1].sum(axis=0))[6:]
+df1["2"] = list(df[df["Y_Class"]==2].sum(axis=0))[6:]
+# df1["total"] = df1.sum(axis=1)
+df1 = df1.transpose()
+df1.to_csv("notNull count by y_class.csv", index = True)
+
+dfList = df1.values.tolist()
+dfListCol = df1.columns.tolist()
+dfListIndex = df1.index.tolist()
+
+dfDict = {k:v for k,v in zip(dfListIndex, dfList)}
+dfDictL = {str(v):k for k,v in zip(dfListIndex, dfList)}
+
+dfDictL = dict()
+for k,v in zip(dfListIndex, dfList):
+    try:
+        dfDictL[str(v)].append(k)
+    except:
+        dfDictL[str(v)] = [k]
+
+temp = [key[1:-1].replace(',', '').split(' ') for key in dfDictL.keys()]
+temp2 = list()
+for t in temp:
+    temp2.append([float(t[0]), float(t[1]), float(t[2])])
+    
+
+gif = pd.DataFrame(temp2, columns = ["0","1","2"])
+gif.plot.barh()
+
+fig, ax = plt.subplots(3,1, figsize=(30,10))
+
+gif = gif.transpose()
+gif.columns = [str(i) for i in range(30)]
+gif.to_csv("asdf.csv")
 
 
-
-
-
-
-
-
-
+sns.set_palette("ch:start=.2,rot=-.3")
+gif.iloc[:10].plot.bar()
+gif.iloc[10:20].plot.bar()
+gif.iloc[20:30].plot.bar()
+plt.show()
 
